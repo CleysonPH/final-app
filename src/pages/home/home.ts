1f3b8ca40af05797b75de6116198c0c6c62f19b2
CleysonPH <cleysonph@gmail.com>
2017-09-27 09:08:28 -0300

    Final Project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { DataProvider } from './../../providers/data/data';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,83 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('slides') slides: any;
 
+  userName: String;
+
+  hasAnswered: boolean = false;
+  correctAnswers: number = 0;
+  wrongAnswers: number = 0;
+
+  slideOptions: any;
+  questions: any;
+
+  constructor(public navCtrl: NavController, public dataService: DataProvider) {
+
+  }
+
+  ionViewDidLoad() {
+    this.slides.lockSwipes(true);
+
+    this.dataService.load().then((data) => {
+
+      data.map((question) => {
+
+        let originalOrder = question.answers;
+        question.answers = this.randomizeAnswers(originalOrder);
+        return question;
+
+      });
+
+      this.questions = data;
+
+    });
+  }
+
+  nextSlide(){
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
+  }
+
+  selectAnswer(answer, question){
+
+    this.hasAnswered = true;
+    answer.selected = true;
+    question.flashCardFlipped = true;
+
+    if(answer.correct){
+      this.correctAnswers++;
+    } else {
+      this.wrongAnswers++;
+    }
+
+    setTimeout(() => {
+      this.hasAnswered = false;
+      this.nextSlide();
+      answer.selected = false;
+      question.flashCardFlipped = false;
+    }, 3000);
+  }
+
+  randomizeAnswers(rawAnswers: any[]): any[]{
+
+    for(let i = rawAnswers.length - 1; i > 0; i--)    {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = rawAnswers[i];
+      rawAnswers[i] = rawAnswers[j];
+      rawAnswers[j] = temp;
+    }
+
+    return rawAnswers;
+  }
+
+  restartQuiz() {
+    this.correctAnswers = 0;
+    this.wrongAnswers = 0;
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(0, 1000);
+    this.slides.lockSwipes(true);
   }
 
 }
